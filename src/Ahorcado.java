2f6e21baa08112e89d7292c9e254606fa5d3ac63
alumno <alumno@aula1-11>
2019-10-25 13:41:08 +0200

    Inicio proyecto ahorcado

---
+++ src/Ahorcado.java
@@ -0,0 +1,16 @@
+
+public class Ahorcado {
+
+	public static void main(String[] args) {
+		System.out.println("JUEGO DEL AHORCADO");
+		System.out.println("_______");
+		System.out.println(" |   |");
+		System.out.println(" |   O");
+		System.out.println(" |  /|/");
+		System.out.println(" |  / /");
+		System.out.println(" |");
+		System.out.println("_|_________");
+
+	}
+
+}
