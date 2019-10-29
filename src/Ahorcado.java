0622ff1da3d7133a3875bafa816fe01ae1837129
alumno <alumno@aula1-11>
2019-10-29 14:30:46 +0100

    Vector creado

--- src/Ahorcado.java
+++ src/Ahorcado.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
 
 public class Ahorcado {
 
 	public static void main(String[] args) {
+		Scanner leer;
+		leer = new Scanner(System.in);
+		
+		int tamaño =0;
+		String palabra = "hola";
+		char vPalabra[] = new char[4];
+		
 		System.out.println("JUEGO DEL AHORCADO");
 		System.out.println("_______");
 		System.out.println(" |   |");
 		System.out.println(" |   O");
 		System.out.println(" |  /|/");
+		System.out.println(" |   |");
 		System.out.println(" |  / /");
 		System.out.println(" |");
 		System.out.println("_|_________");
-
+		
+		for(int i=0;i<palabra.length();i++) {
+			
+		}
 	}
 
 }
